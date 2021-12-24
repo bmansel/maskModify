27e487eca81e06225a8865415f7dc1108eb69112
bmansel <bmansel@gmail.com>
2021-12-24 16:12:45 +0800

    tidy and fix close after open()

--- maskModifier.py
+++ maskModifier.py
@@ -20,27 +20,37 @@ import os
 import numpy as np
 import argparse
 
+def saveMask(maskPixelsX, maskPixelsY, maskValue, experimentDirectory, saveName, overwrite):
+    if overwrite is True:
+        f=open(experimentDirectory + "/" + saveName, 'w')
+        np.savetxt(f, np.transpose([maskPixelsX, maskPixelsY, maskValue]),fmt='%11u%11u%11.6f')
+        f.close()
+        return
+    elif overwrite is False:
+        f=open(experimentDirectory + "/" + saveName, 'a')
+        np.savetxt(f, np.transpose([maskPixelsX, maskPixelsY, maskValue]),fmt='%11u%11u%11.6f')
+        f.close()
+        return
+
 
 def makeMaskFile(experimentDirectory, maskName, saveName, overwrite):
     mask2d = np.load(experimentDirectory+ "/" + maskName)
+    mask_sum = np.sum(mask2d)
     maskPixels = np.squeeze(np.where(1 == mask2d))
-    maskValue = [1.000000]*len(maskPixels[0])
+    if mask_sum <= 1:
+        print()
+        print("Only 1 or less pixels found. no mask created, please assign more than one pixel.")
+        return
 
-    if overwrite is True:
-        f=open(experimentDirectory + "/" + saveName, 'w')
-        np.savetxt(f, np.transpose([maskPixels[1], maskPixels[0], maskValue]),fmt='%11u%11u%11.6f')
-    elif overwrite is False:
-        try:
-            f=open(experimentDirectory + "/" + saveName, 'a')
-            np.savetxt(f, np.transpose([maskPixels[1], maskPixels[0], maskValue]),fmt='%11u%11u%11.6f')
-        except:
-            print('Could not open' + experimentDirectory + "/" + saveName)
+    maskValue = np.array([1.000000]*len(maskPixels[0]))
+    saveMask(maskPixels[1], maskPixels[0], maskValue, experimentDirectory, saveName, overwrite)
+    
 
 def main():
     parser = argparse.ArgumentParser(description='Change mask from 2d pyFAI image mask to 1d reject file.')
     parser.add_argument('--directory', action='store', help='directory where the experiment is, if none default is cwd [default= None]', type=str, default= None)
     parser.add_argument('--pyFAI_mask', action='store', help='name of 2d mask file .npy from pyFAI [default=mask.npy]',type=str, default='mask.npy')
-    parser.add_argument('--save_name', action='store', help='name of column .dat file to save [default=reject.dat].',type=str, default='reject.dat')
+    parser.add_argument('--save_name', action='store', help='name of column .dat file to save [default=reject.dat].',type=str, default='REJECT.dat')
     parser.add_argument('--overwrite', action='store_true', help='If --overwrite reject.dat will be overwritten. otherwise data is appended to old file')
     args = parser.parse_args()
     if args.directory is None:
